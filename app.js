31c42396ad9c59e7e6fe33eabd25b700995f3918
Francis Chen <fncis.chen@gmail.com>
2019-04-25 13:21:36 -0700

    updated app

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const app = express();
+
+
+
+
+
+
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+app.get('/', (req, res,) => {
+    res.send(`sanity check success`)
+});
+
+
+module.exports = app;
