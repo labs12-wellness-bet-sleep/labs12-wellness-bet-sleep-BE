9f5a390a9c16eaa8c194dcacc50bdab789aae81f
txiong000 <tkx422@gmail.com>
2019-04-29 16:10:08 -0500

    refactored login function, moved into userRoutesjs

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -55,4 +55,27 @@ usersRouter.get("/:id", async (req, res) => {
     }
   });
 
+  usersRouter.post("/login", (req, res) => {
+    let { username, password } = req.body;
+    if (username && password ) {
+      Users.login({ username })
+      .first()
+      .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+    } else {
+      res.status(401).json({message: "Invalid username or password"});
+    }
+
+  })
+
+ 
+
 module.exports = usersRouter
