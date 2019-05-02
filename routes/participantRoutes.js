ee4bc5e4f038c66d8e4b377905d2ebd150a2889e
txiong000 <tkx422@gmail.com>
2019-05-02 15:42:41 -0500

    added delete end point for participant

--- routes/participantRoutes.js
+++ routes/participantRoutes.js
@@ -1,14 +1,29 @@
 const participantRouter = require("express").Router();
 const userdb = require("../database/dbConfig.js");
-const db = require("../models/participants.js");
+const Participant = require("../models/participants.js");
+const Groups = require("../models/groups.js");
 
-participantRouter.get("/:id", async (req, res) => {
+
+
+participantRouter.get("/", (req, res) => {
+    Participant.findParticipant()
+               .then(participant => {
+                   res.status(200).json(participant)
+               })
+               .catch(error => {
+                   res.status(500).send(error)
+               })
+})
+
+participantRouter.get("/:id/participant", async (req, res) => {
     try {   
-            let id = req.params.id;
+            let {id} = req.params;
             console.log(id)
             if(id){
-               const participant = await db.findParticipantsByGroup(id)
-               res.status(200).json(participant);
+               const group = await Groups.findGroupById(id);
+               console.log(group)
+               const participant = await Participant.findParticipantsByGroup(id)
+               res.status(200).json({...group, participant});
             } else {
                 res.status(400).json({message:`Group with id:${id} does not exist `})
             }
@@ -16,6 +31,20 @@ participantRouter.get("/:id", async (req, res) => {
       
     } catch (error) {
         res.status(500).send(error.message);
+    }
+})
+
+participantRouter.delete("/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        if(id) {
+            let participant = await Participant.delParticipant(id);
+            res.status(200).json(participant)
+        } else {
+            res.status(400).json({message: "No participant by that id"})
+        }
+    } catch(error) {
+        res.status(500).send(error.message)
     }
 })
 
