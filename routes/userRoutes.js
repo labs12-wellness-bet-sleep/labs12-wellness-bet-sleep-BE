7d8cb6570c6e10952915dfd08d13e693fd4540f6
Francis Chen <fncis.chen@gmail.com>
2019-05-02 14:35:29 -0700

    modified migrations on users table, modified users data model & router, added knex global

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -73,7 +73,7 @@ usersRouter.get("/:id", fb.isAuthenticated, async (req, res) => {
         .first()
         .then(user => {
         if (user && bcrypt.compareSync(password, user.password)) {
-          res.status(200).json({ message: `Welcome ${user.email}!` });
+          res.status(200).json({ message: `Welcome ${user.fullName}!` });
         } else {
           res.status(401).json({ message: 'Invalid Credentials' });
         }
