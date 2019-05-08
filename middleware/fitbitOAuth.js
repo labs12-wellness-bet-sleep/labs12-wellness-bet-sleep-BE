8685338e485a674622d6206f4478c13008f5f7b5
Francis Chen <fncis.chen@gmail.com>
2019-05-08 10:59:03 -0700

    set up way to retrieve user id

--- middleware/fitbitOAuth.js
+++ middleware/fitbitOAuth.js
@@ -1,6 +1,7 @@
 const fitBitOAuthRouter = require("express").Router();
 
 // Declare the redirect route
+// source: https://www.sohamkamani.com/blog/javascript/2018-06-24-oauth-with-node-js/
 fitBitOAuthRouter.get('/oauth/redirect', (req, res) => {
     // The req.query object has the query params that
     // were sent to this route. We want the `code` param
@@ -16,8 +17,12 @@ fitBitOAuthRouter.get('/oauth/redirect', (req, res) => {
     }).then((response) => {
       // Once we get the response, extract the access token from
       // the response body
-      const accessToken = response.data.access_token
+      const accessToken = response.data.access_token;
+      const user_id = response.data.user_id;
       // redirect the user to the welcome page, along with the access token
-      res.redirect(`/groupDashboard?access_token=${accessToken}`)
+      res.redirect(`/groupDashboard?access_token=${accessToken}`);
+      res.status(200).json({user_id: user_id});
     })
   })
+
+  export default fitBitOAuthRouter;
